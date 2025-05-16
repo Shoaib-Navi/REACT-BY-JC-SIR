42f365863922c8f1a9b0cd9a4e994fd1fce2cc0d
Shoaib Navi <147228272+Shoaib-Navi@users.noreply.github.com>
2025-05-16 15:59:00 +0530

    Initial React project setup

---
+++ my-app/src/Components/module1.mjs
@@ -0,0 +1,7 @@
+const a ="hello";
+const b ="world";
+const c ="shoaib";
+
+export default b; //default export it give only b
+export {a};  //name export it give content whuich the variable content
+export {c};
---
+++ my-app/src/Components/module2.mjs
@@ -0,0 +1,4 @@
+import x , {a,c} from './module1.mjs';
+
+//import from './module1.mjs';
+console.log(x,a,c);  //in default export no need to make the same name
