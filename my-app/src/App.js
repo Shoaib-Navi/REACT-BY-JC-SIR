57dba52b1893dea86e9ff2354d3dfac73ce62aeb
Shoaib Navi <147228272+Shoaib-Navi@users.noreply.github.com>
2025-05-23 15:48:36 +0530

    adding some comments

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Header from './MyComponents/Header';
+import TextForm from './MyComponents/TextForm';
+
 
 function App() {
   return (
-    <Header name="BlackShip" about="about" title="TextUtils" aboutText="AboutTextUtils" searchBar={false} />  //by giving searchBar=true/false you can show or hide search bar
-    
+    <>
+    <Header name="BlackShip" about="about" title="TextUtils" aboutText="AboutTextUtils" searchBar={false} />  {/*by giving searchBar=true/false you can show or hide search bar*/}
+    <TextForm />
+    </>
     
   );
 }
--- my-app/src/MyComponents/TextForm.js
+++ my-app/src/MyComponents/TextForm.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React from 'react';
 
 export default function TextForm() {
-  return (<>
-      <div ClassName="form-floating mb-3">
-  <input type="email" ClassName="form-control" id="floatingInput" placeholder="name@example.com">
-  <label for="floatingInput">Email address</label>
-</div>
-<div ClassName="form-floating">
-  <input type="password" ClassName="form-control" id="floatingPassword" placeholder="Password">
-  <label for="floatingPassword">Password</label>
-</div>
-   </>
-  )
+  return (
+    <div className="container d-flex justify-content-center mt-5">
+      <div className="col-md-6 col-lg-4">
+        <div className="form-floating mb-3">
+          <input
+            type="email"
+            className="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label htmlFor="floatingInput">Email address</label>
+        </div>
+        <div className="form-floating">
+          <input
+            type="password"
+            className="form-control"
+            id="floatingPassword"
+            placeholder="Password"
+          />
+          <label htmlFor="floatingPassword">Password</label>
+        </div>
+      </div>
+    </div>
+  );
 }
